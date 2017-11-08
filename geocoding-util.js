bccf5176c05c33f35b04b93b3f0ebdd5bd540d31
yi.liu <yi.liu@akqa.com>
2017-11-08 00:30:04 -0500

    geocoding updates

--- geocoding-util.js
+++ geocoding-util.js
@@ -2,3 +2,9 @@ const mapsAPIurl = 'https://maps.googleapis.com/maps/api/geocode/json';
 const mapsAPIkey = 'AIzaSyBVqsCgzlFaE8wxWXMtkC5gzJ7Wb1KgsUE';
 
 const stationlocation = require('./data/stationlocation.json');
+
+var GeocodingUtil = {
+
+};
+
+module.exports = GeocodingUtil;
