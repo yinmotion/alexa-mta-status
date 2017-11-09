a5bcda69be97f02afea1fb6b0da250bbb5334baf
yi.liu <yi.liu@akqa.com>
2017-11-09 00:27:50 -0500

    Added get station address class

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const GeocodingUtil = require('./geocoding-util');
 const apiKey = '6c2cdead4118f35fe1ed1a3604e37ffb';
 
 var feedId = '1';
+var trainLine = '4';
 
 var requestSettings = {
   method: 'GET',
@@ -19,12 +20,12 @@ var requestSettings = {
 
 var GTFSrealtimeService = {
   checkStation: function(line, direction){
-    line = line.toUpperCase();
-    console.log("line = " + line);
+    trainLine = line.toUpperCase();
+    console.log("line = " + trainLine);
     console.log("direction = " + direction);
 
     for(var idObj of feedIDs){
-      if(idObj.trainLines.includes(line)){
+      if(idObj.trainLines.includes(trainLine)){
         feedId = idObj.id;
         break;
       }
@@ -34,6 +35,8 @@ var GTFSrealtimeService = {
     console.log("feed_id = " + feedId);
     console.log("requestSettings.url = " + requestSettings.url);
     console.log("requestSettings.method = " + requestSettings.method);
+
+    this.getFeed();
   },
 
   getFeed : function(){
@@ -49,7 +52,7 @@ var GTFSrealtimeService = {
              entity.trip_update.stop_time_update.forEach(function(update, index) {
               var route_id = entity.trip_update.trip.route_id;
 
-              if(update.stop_id === '635N' && route_id === "6"){
+              if(update.stop_id === '635S' && route_id === trainLine){
                 console.log("stop ID = " + update.stop_id);
                 var date = new Date(0);
                 var curDate = new Date();
@@ -91,59 +94,6 @@ var GTFSrealtimeService = {
   }
 
 };
-
-/*
-request(requestSettings, function(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
-    var aArrivals = [];
-    feed.entity.forEach(function(entity) {
-      if (entity.trip_update) {
-        //console.log(entity.trip_update.trip.route_id);
-
-         entity.trip_update.stop_time_update.forEach(function(update, index) {
-          var route_id = entity.trip_update.trip.route_id;
-
-          if(update.stop_id === '635N' && route_id === "6"){
-            console.log("stop ID = " + update.stop_id);
-            var date = new Date(0);
-            var curDate = new Date();
-            if(update.arrival){
-            date.setUTCSeconds(update.arrival.time);
-
-
-
-            console.log("arrival | " + date);
-            var arrivalInMins = (date.getTime()-curDate.getTime())/60000;
-            console.log("time diff = " + arrivalInMins);
-
-            aArrivals.push(arrivalInMins);
-
-          }
-          /*
-          date = new Date(0);
-          if(update.departure){
-            date.setUTCSeconds(update.departure.time);
-
-            console.log("departure | " + date);
-          }
-
-          console.log("====================================================================")
-          };
-
-        })
-      }
-
-
-    });
-      aArrivals = aArrivals.sort(function (a, b) {  return a - b;  });
-      console.log(aArrivals);
-  }else{
-    console.log(error);
-    console.log(response)
-  }
-});
-*/
 
 module.exports = GTFSrealtimeService;
 
---
+++ data/getStationAddress.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const fs = require("fs");
+const request = require('request');
+
+const mapsAPIurl = 'https://maps.googleapis.com/maps/api/geocode/json';
+const mapsAPIkey = 'AIzaSyBVqsCgzlFaE8wxWXMtkC5gzJ7Wb1KgsUE';
+
+var stations;
+var newStations = [];
+
+var index = 0;
+
+var requestSettings = {
+    method: 'GET',
+    encoding: null
+};
+
+fs.readFile('./stationlocation.json', (err, data) => {
+    if(err) throw err;
+    stations = JSON.parse(data);
+
+    readStationData();
+    /*
+    stations.forEach(element => {
+        let latlng = element['GTFS Latitude']+','+element['GTFS Longitude'];
+        //console.log('GTFS Latitude '+ element['GTFS Latitude']);
+        //requestSettings.url = mapsAPIurl + '?key=' + mapsAPIkey + "&latlng="+ latlng;
+        var reqSettings = requestSettings;
+        reqSettings.url = mapsAPIurl + '?key=' + mapsAPIkey + "&latlng="+ latlng;
+        getAddress(element, reqSettings);
+
+        
+        //aStations.push(element);
+        //console.log(element['GTFS Latitude']);
+    });
+    /*
+    let newStations = JSON.stringify(aStations);
+    fs.writeFile('./stations.json', newStations, (err)=>{
+        if(err) throw err;
+        console.log('written to file');
+    });
+    */
+});
+
+var readStationData = function(){
+    if(index<stations.length){
+        var element = stations[index];
+        let latlng = element['GTFS Latitude']+','+element['GTFS Longitude'];
+        var reqSettings = requestSettings;
+        reqSettings.url = mapsAPIurl + '?key=' + mapsAPIkey + "&latlng="+ latlng;
+        getAddress(element, reqSettings);
+    }else{
+        let data = JSON.stringify(newStations);
+        fs.writeFile('./stations.json', data, (err)=>{
+            if(err) throw err;
+            console.log('written to file');
+        });
+        console.log('Complete');
+    }
+};
+
+var getAddress = function(element, reqSettings){
+    request(reqSettings, function(error, response, body) {
+        let obj = JSON.parse(body);
+        if (!error && response.statusCode == 200) {
+            let status = obj.status;
+            console.log('status = '+status);
+            if(status !== 'OK')
+            return;
+            let result = obj.results[0];
+            console.log('result = '+result);
+            
+            if(result!=null && result!=undefined){
+                element['address']=result.formatted_address;
+                newStations.push(element);
+
+                let data = JSON.stringify(newStations, 2);
+                
+                console.log(data);
+                console.log('------------------------------------------------------------------------');      
+            }else{
+                console.log('!!! Latitude : ' + element['GTFS Latitude'] + " | no result !!!");       
+            }
+            index++;
+            setTimeout(readStationData, 300);
+        }
+    })
+}
+
+
+
+
