f0f1321ed98b57336fad9f19e1c925ed542cb822
yi.liu <yi.liu@akqa.com>
2017-11-11 00:48:55 -0500

    parse station data by borough

--- data/__getStationAddress.js
+++ data/__getStationAddress.js
@@ -17,7 +17,7 @@ var requestSettings = {
 };
 
 fs.readFile('./stationlocation.json', (err, data) => {
-    if(err) throw err;
+    if (err) throw err;
     stations = JSON.parse(data);
 
     readStationData();
@@ -43,52 +43,48 @@ fs.readFile('./stationlocation.json', (err, data) => {
     */
 });
 
-var readStationData = function(){
-    if(index<stations.length){
+var readStationData = function () {
+    if (index < stations.length) {
         var element = stations[index];
-        let latlng = element['GTFS Latitude']+','+element['GTFS Longitude'];
+        let latlng = element['GTFS Latitude'] + ',' + element['GTFS Longitude'];
         var reqSettings = requestSettings;
-        reqSettings.url = mapsAPIurl + '?key=' + mapsAPIkey + "&latlng="+ latlng;
+        reqSettings.url = mapsAPIurl + '?key=' + mapsAPIkey + "&latlng=" + latlng;
         getAddress(element, reqSettings);
-    }else{
-        let data = JSON.stringify(newStations);
-        fs.writeFile('./stations.json', data, (err)=>{
-            if(err) throw err;
-            console.log('written to file');
-        });
+    } else {
+
         console.log('Complete');
     }
 };
 
-var getAddress = function(element, reqSettings){
-    request(reqSettings, function(error, response, body) {
+var getAddress = function (element, reqSettings) {
+    request(reqSettings, function (error, response, body) {
         let obj = JSON.parse(body);
         if (!error && response.statusCode == 200) {
             let status = obj.status;
-            console.log('status = '+status);
-            if(status !== 'OK')
-            return;
+            console.log('status = ' + status);
+            if (status !== 'OK')
+                return;
 
             let result = obj.results[0];
-            console.log('result = '+result);
-            
-            if(result!=null && result!=undefined){
-                element['address']=result.formatted_address;
+            console.log('result = ' + result);
+
+            if (result != null && result != undefined) {
+                element['address'] = result.formatted_address;
                 newStations.push(element);
 
                 let data = JSON.stringify(newStations, null, '\t');
-                
-                console.log(data);
-                console.log('------------------------------------------------------------------------');      
-            }else{
-                console.log('!!! Latitude : ' + element['GTFS Latitude'] + " | no result !!!");       
+
+                fs.writeFile('./stations.json', data, (err) => {
+                    if (err) throw err;
+                    console.log('written to file');
+                });
+                //console.log(data);
+                console.log('------------------------------------------------------------------------');
+            } else {
+                console.log('!!! Latitude : ' + element['GTFS Latitude'] + " | no result !!!");
             }
             index++;
-            setTimeout(readStationData, 300);
+            setTimeout(readStationData, 100);
         }
     })
 }
-
-
-
-
--- geocoding-util.js
+++ geocoding-util.js
@@ -1,12 +1,12 @@
 const mapsAPIurl = 'https://maps.googleapis.com/maps/api/geocode/json';
 const mapsAPIkey = 'AIzaSyBVqsCgzlFaE8wxWXMtkC5gzJ7Wb1KgsUE';
 
-const stationlocation = require('./data/stationlocation.json');
+const stationlocation = require('./data/stations-by-borough.json');
 
 var GeocodingUtil = {
     reverseGeoCoding : function(lat, long){
         
-    } 
+    }
 };
 
 module.exports = GeocodingUtil;
